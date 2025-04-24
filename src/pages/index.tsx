1fdfaeee6e742346d0d27dbd9f89c5ee83baa9be
nasimkhan11 <nashim@technojade.com>
2025-04-24 23:41:17 +0530

    second commit

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,7 @@
 import { NextPage } from 'next';
 import { useGetProductQuery } from '../services/productsApi';
+import { wrapper } from '../store/store';
+import { productsApi } from '../services/productsApi';
 
 const Home: NextPage = () => {
   const { data, error, isLoading } = useGetProductQuery(1);
@@ -14,5 +16,17 @@ const Home: NextPage = () => {
     </main>
   );
 };
+
+
+export const getServerSideProps = wrapper.getServerSideProps(
+  (store) => async () => {
+    // 1. Initiate the query
+    store.dispatch(productsApi.endpoints.getProduct.initiate(1));
+    // 2. Await all running queries via the util namespace
+    await Promise.all(store.dispatch(productsApi.util.getRunningQueriesThunk()));
+    return { props: {} };
+  }
+);
+
 
 export default Home;
--- src/store/store.ts
+++ src/store/store.ts
@@ -1,15 +1,19 @@
 import { configureStore } from '@reduxjs/toolkit';
-import { createWrapper } from 'next-redux-wrapper';
-import { productsApi } from '../services/productsApi';
+import { createWrapper }   from 'next-redux-wrapper';
+import { productsApi }      from '../services/productsApi';
 
+// 1. Factory for per-request stores
 export const makeStore = () =>
   configureStore({
     reducer: {
       [productsApi.reducerPath]: productsApi.reducer,
     },
-    middleware: (getDefaultMiddleware) =>
-      getDefaultMiddleware().concat(productsApi.middleware),
+    middleware: (gDM) => gDM().concat(productsApi.middleware),
   });
 
+// 2. Create a singleton for local imports (optional)
+export const store = makeStore();
+
+// 3. Wrapper for Next.js SSR/SSG
 export type AppStore = ReturnType<typeof makeStore>;
 export const wrapper = createWrapper<AppStore>(makeStore);
